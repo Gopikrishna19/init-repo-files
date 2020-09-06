f878dc65a1a5d1108d347eb9ef5634e26a7ac4a3
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2020-09-06 10:25:20 -0500

    use templates to include the missed files

--- index.js
+++ index.js
@@ -1,30 +1,43 @@
 const fs = require('fs');
 const path = require('path');
 
-const files = [
-    '.editorconfig',
-    '.gitignore',
-];
+const files = [];
+const maps = files.reduce((map, file) => Object.assign(map, {
+    [file]: file,
+}), {
+    '.editorconfig': 'editorconfig.ini',
+    '.gitignore': 'gitignore.txt',
+});
 
-files.forEach(file => {
+const handleError = (file, err) => {
+    console.log(`Something went wrong when writing ${file}.`);
+    console.error(err);
+    process.exitCode = 1;
+};
+
+const createErrorHandler = file => error => handleError(file, error);
+
+Object.entries(maps).forEach(([file, template]) => {
     const destination = path.join(process.cwd(), file);
+    const source = path.join(__dirname, 'templates', template);
 
     fs.stat(destination, (err, stats) => {
         if (err) {
             if (err.code === 'ENOENT') {
-                const source = path.join(__dirname, file);
+                const readStream = fs.createReadStream(source);
                 const writeStream = fs.createWriteStream(destination);
+                const errorHandler = createErrorHandler(file);
 
-                fs.createReadStream(source)
-                    .pipe(writeStream);
+                readStream.on('error', errorHandler);
+                writeStream.on('error', errorHandler);
+
+                readStream.pipe(writeStream);
 
                 writeStream.on('finish', () => {
                     console.log(`Wrote ${file} successfully.`);
                 });
             } else {
-                console.log(`Something went wrong when writing ${file}.`);
-                console.error(err);
-                process.exitCode = 1;
+                handleError(file, err);
             }
         } else {
             console.log(`Skipped ${file} as it already exists.`);
